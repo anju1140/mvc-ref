2cf4a38c7a0119b6501f84304f7c3385c5492f39
anju1140 <anju114@outlook.com>
2022-04-28 16:49:53 +0530

    Spring data JPA added

--- src/main/java/com/tanvi/ref/mvcref/MvcRefApplication.java
+++ src/main/java/com/tanvi/ref/mvcref/MvcRefApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MvcRefApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MvcRefApplication.class, args);
 	}
