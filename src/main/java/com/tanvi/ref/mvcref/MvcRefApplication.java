8f2f64c82be5546f6e82ac6432f1c69b5465c0c1
anju1140 <anju114@outlook.com>
2022-04-28 12:51:29 +0530

    Spring boot Web Demo - Init

---
+++ src/main/java/com/tanvi/ref/mvcref/MvcRefApplication.java
@@ -0,0 +1,13 @@
+package com.tanvi.ref.mvcref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcRefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanvi/ref/mvcref/MvcRefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanvi.ref.mvcref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcRefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
